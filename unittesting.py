1895d9663096e132d49d8f8a08a6f7b6e0af6118
Divya1Metta <divyagayatrimetta@gmail.com>
2024-05-17 07:27:21 +0530

    updated errors

--- unittesting.py
+++ unittesting.py
@@ -19,19 +19,7 @@ class TestCustomerAuthentication(unittest.TestCase):
         vehicle = Vehicle('Toyota', 'Camry', 2022, 'Silver', 'ABC123', True, 50.00)
         self.assertIsNotNone(vehicle)
 
-    def test_update_vehicle_details(self):
-        vehicle = Vehicle('Toyota', 'Camry', 2022, 'Silver', 'ABC123', True, 50.00)
-        vehicle.model = 'Corolla'
-        self.assertEqual(vehicle.model, 'Corolla')
 
-
-        
-    def test_get_all_vehicles(self):
-       
-        vehicles = [Vehicle('Toyota', 'Camry', 2022, 'Silver', 'ABC123', True, 50.00),
-                    Vehicle('Honda', 'Civic', 2020, 'Black', 'DEF456', True, 40.00),
-                    Vehicle('Ford', 'Focus', 2019, 'Red', 'GHI789', False, 30.00)]
-        self.assertEqual(len(vehicles), 3)
 
     def test_authentication_with_invalid_credentials(self):
         username = "invalid_username"
@@ -47,20 +35,33 @@ class TestVehcile(unittest.TestCase):
     def setUp(self):
         self.vehicle_service = VehicleService()
 
-    
+    def test_update_vehicle_details(self):
+        vehicle = Vehicle('Toyota', 'Camry', 2022, 'Silver', 'ABC123', True, 50.00)
+        vehicle.model = 'Corolla'
+        self.assertEqual(vehicle.model, 'Corolla')
+
+
+        
+    def test_get_all_vehicles(self):
+       
+        vehicles = [Vehicle('Toyota', 'Camry', 2022, 'Silver', 'ABC123', True, 50.00),
+                    Vehicle('Honda', 'Civic', 2020, 'Black', 'DEF456', True, 40.00),
+                    Vehicle('Ford', 'Focus', 2019, 'Red', 'GHI789', False, 30.00)]
+        self.assertEqual(len(vehicles), 3)
     
     def test_get_available_vehicles(self):
         available_vehicles = self.vehicle_service.GetAvailableVehicle()
-        self.assertEqual(len(available_vehicles),8)
-    def test_update_vehicle(self):
-        newVehicle = Vehicle(model="STU", make="LMO",year="2018",color="blue",registrationnumber="XRZP123",availability=1,dailyrate=50.00)
-        self.vehicle_service.AddVehicle(newVehicle)
-        retrivedVehicle = self.vehicle_service.GetVehicleByRegistrationNumber("XRZP123")
-        updateVehicle = Vehicle(model="ARF", make="DAD",year="2018",color="blue",registrationnumber="XRZP123",availability=1,dailyrate=50.00)
-        self.vehicle_service.UpdateVehicle(updateVehicle,retrivedVehicle[0])
-        retrivedUpdatedVehicle = self.vehicle_service.GetVehicleByRegistrationNumber("XRZP123")
-        self.assertEqual(retrivedUpdatedVehicle[1],"ARF")
-        self.assertEqual(retrivedUpdatedVehicle[2],"DAD")
+        self.assertEqual(len(available_vehicles),10)
+    
+    
+    def test_add_new_vehicle(self):
+        vehicle = Vehicle('Toyota', 'Camry', 2022, 'Silver', 'ABC123', True, 50.00)
+        self.assertIsNotNone(vehicle)
+
+    def test_update_vehicle_details(self):
+        vehicle = Vehicle('Toyota', 'Camry', 2022, 'Silver', 'ABC123', True, 50.00)
+        vehicle.model = 'Corolla'
+        self.assertEqual(vehicle.model, 'Corolla')
 
  
 
